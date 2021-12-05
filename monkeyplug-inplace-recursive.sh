52220a9cfef5f60b076d08f6dc41e4eff7d39524
SG <13872653+mmguero@users.noreply.github.com>
2021-12-05 16:49:42 -0700

    look for more file types

--- monkeyplug-inplace-recursive.sh
+++ monkeyplug-inplace-recursive.sh
@@ -4,7 +4,7 @@ PROCESSES="${MONKEYPLUG_PROCESS_COUNT:-1}"
 
 find . -type f -print0 | xargs -0 -P $PROCESSES -I AUDIO bash -c '
   if ! fuser -s "AUDIO" 2>/dev/null; then
-    if file --mime-type "AUDIO" | grep -q ": audio/"; then
+    if file --mime-type "AUDIO" | grep -Pq ": (audio/|video/|application/octet-stream)"; then
       pushd "$(dirname "AUDIO")" >/dev/null 2>&1
       monkeyplug-inplace.sh "$(basename "AUDIO")"
       popd >/dev/null 2>&1
