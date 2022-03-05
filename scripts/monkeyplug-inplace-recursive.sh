2c23c8eeafc7c4b6163418f0b92ec25cca798faa
Seth Grover <mero.mero.guero@gmail.com>
2022-03-05 15:21:35 -0700

    don't run without fuser

--- scripts/monkeyplug-inplace-recursive.sh
+++ scripts/monkeyplug-inplace-recursive.sh
@@ -2,12 +2,16 @@
 
 PROCESSES="${MONKEYPLUG_PROCESS_COUNT:-1}"
 
-find . -type f -print0 | xargs -0 -P $PROCESSES -I AUDIO bash -c '
-  if ! fuser -s "AUDIO" 2>/dev/null; then
-    if file --mime-type "AUDIO" | grep -Pq ": (audio/|video/|application/octet-stream)"; then
-      pushd "$(dirname "AUDIO")" >/dev/null 2>&1
-      monkeyplug-inplace.sh "$(basename "AUDIO")"
-      popd >/dev/null 2>&1
+if type fuser >/dev/null 2>&1; then
+  find . -type f -print0 | xargs -0 -P $PROCESSES -I AUDIO bash -c '
+    if ! fuser -s "AUDIO" 2>/dev/null; then
+      if file --mime-type "AUDIO" | grep -Pq ": (audio/|video/|application/octet-stream)"; then
+        pushd "$(dirname "AUDIO")" >/dev/null 2>&1
+        monkeyplug-inplace.sh "$(basename "AUDIO")"
+        popd >/dev/null 2>&1
+      fi
     fi
-  fi
-'
+  '
+else
+  echo 'missing fuser, install psmisc' >&2
+fi
