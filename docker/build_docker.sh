a9f7cefe8b677e50f6f95f69f77d4e6af16cf834
Seth Grover <mero.mero.guero@gmail.com>
2022-05-10 22:11:32 -0600

    adjust scripts to allow podman or docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -5,10 +5,12 @@ set -o pipefail
 
 ENCODING="utf-8"
 
+ENGINE="${CONTAINER_ENGINE:-docker}"
+
 [[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
 [[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
-if ! (type "$REALPATH" && type "$DIRNAME" && type docker) > /dev/null; then
-  echo "$(basename "${BASH_SOURCE[0]}") requires docker, $REALPATH and $DIRNAME"
+if ! (type "$REALPATH" && type "$DIRNAME" && type "$ENGINE") > /dev/null; then
+  echo "$(basename "${BASH_SOURCE[0]}") requires $ENGINE, $REALPATH and $DIRNAME"
   exit 1
 fi
 export SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
@@ -16,9 +18,9 @@ export SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
 pushd "$SCRIPT_PATH"/.. >/dev/null 2>&1
 
 if [[ -n "$VOSK_MODEL_URL" ]]; then
-  docker build -f docker/Dockerfile --build-arg VOSK_MODEL_URL="$VOSK_MODEL_URL" -t ghcr.io/mmguero/monkeyplug .
+  $ENGINE build -f docker/Dockerfile --build-arg VOSK_MODEL_URL="$VOSK_MODEL_URL" -t ghcr.io/mmguero/monkeyplug .
 else
-  docker build -f docker/Dockerfile -t ghcr.io/mmguero/monkeyplug:small .
+  $ENGINE build -f docker/Dockerfile -t ghcr.io/mmguero/monkeyplug:small .
 fi
 
 popd >/dev/null 2>&1
--- docker/monkeyplug-docker.sh
+++ docker/monkeyplug-docker.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-export MONKEYPLUG_IMAGE="${MONKEYPLUG_DOCKER_IMAGE:-ghcr.io/mmguero/monkeyplug:small}"
+export MONKEYPLUG_IMAGE="${MONKEYPLUG_IMAGE:-ghcr.io/mmguero/monkeyplug:small}"
+ENGINE="${CONTAINER_ENGINE:-docker}"
+if [[ "$ENGINE" == "podman" ]]; then
+  CONTAINER_PUID=0
+  CONTAINER_PGID=0
+else
+  CONTAINER_PUID=$(id -u)
+  CONTAINER_PGID=$(id -g)
+fi
 
 # run from directory containing audio file
 
-docker run --rm -t \
-  -u $(id -u):$(id -g) \
+$ENGINE run --rm -t \
+  -u $CONTAINER_PUID:$CONTAINER_PGID \
   -v "$(realpath "${PWD}"):${PWD}" \
   -w "${PWD}" \
   "$MONKEYPLUG_IMAGE" "$@"
