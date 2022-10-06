7d4b5b6dc5f1cb33b6886b54d4caa11c008b781f
Seth Grover <mero.mero.guero@gmail.com>
2022-10-06 13:46:07 -0600

    base monkeyplug-inplace.sh on embedded metadata instead of breadcrumb

--- scripts/monkeyplug-inplace.sh
+++ scripts/monkeyplug-inplace.sh
@@ -17,34 +17,25 @@ fi
 for AUDIOFILE in "$@"; do
   if [[ -f "${AUDIOFILE}" ]] && [[ "${AUDIOFILE}" == *'.'* ]]; then
     pushd "$(dirname "${AUDIOFILE}")" >/dev/null 2>&1
-    # only process audio files that haven't been cleaned before,
-    # based on dropping an empty breadcrumb file in the same location
-    OUTFILE="${AUDIOFILE%.*}.mp3"
-    if [[ "${OUTFILE}" == "${AUDIOFILE}" ]]; then
-      OUTFILE="${AUDIOFILE%.*}_clean.mp3"
-      NEW_FILE_EXTENSION=0
-    else
-      NEW_FILE_EXTENSION=1
-    fi
-    CLEANED_BREADCRUMB=".cleaned_$(echo "${OUTFILE%.*}" | tr -cd '[:alnum:]._-')"
-    if [[ ! -e "${CLEANED_BREADCRUMB}" ]]; then
-      # clean the audio file
-      echo "Processing \"${AUDIOFILE}\"..." >&2
-      ${MONKEYPLUG_BIN} -i "${AUDIOFILE}" -o "${OUTFILE}"
-      # if the cleaned audio file exists and is larger than negligible,
-      # remove the original file and accept the cleaned one
-      if [[ -f "${OUTFILE}" ]]; then
-        OUTFILE_SIZE=$(stat -c%s "${OUTFILE}")
-        if (( ${OUTFILE_SIZE} > 16000 )); then
-          rm -f "${AUDIOFILE}"
-          if (( ${NEW_FILE_EXTENSION} == 0 )); then
-            mv -f "${OUTFILE}" "${AUDIOFILE}" && touch "${CLEANED_BREADCRUMB}"
-          else
-            touch "${CLEANED_BREADCRUMB}"
-          fi
-        fi
+
+    # monkeyplug will only process audio files that haven't been cleaned before,
+    # based on the contents of embedded metadata tags
+    OUTFILE="${AUDIOFILE%.*}_clean.${AUDIOFILE##*.}"
+    echo "Processing \"${AUDIOFILE}\"..." >&2
+    ${MONKEYPLUG_BIN} -i "${AUDIOFILE}" -o "${OUTFILE}" -x MATCH
+
+    # if the cleaned audio file exists and is larger than negligible,
+    # remove the original file and accept the cleaned one. if the output
+    # audio file doesn't exist, something went wrong or it didn't need to
+    # be cleand
+    if [[ -f "${OUTFILE}" ]]; then
+      OUTFILE_SIZE=$(stat -c%s "${OUTFILE}")
+      if (( ${OUTFILE_SIZE} > 16000 )); then
+        rm -f "${AUDIOFILE}"
+        mv -f "${OUTFILE}" "${AUDIOFILE}"
       fi
     fi
+
     popd >/dev/null 2>&1
   fi
 done
