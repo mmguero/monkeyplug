d3c088bb60ce8d0364249b81330d9eb04a782e02
Seth Grover <mero.mero.guero@gmail.com>
2022-10-08 21:32:46 -0600

    added a script for storing the names of scrubbed files in a database

--- scripts/monkeyplug-inplace.sh
+++ scripts/monkeyplug-inplace.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+[[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
+[[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
+if ! (type "$REALPATH" && type "$DIRNAME") > /dev/null; then
+  echo "$(basename "${BASH_SOURCE[0]}") requires $REALPATH and $DIRNAME"
+  exit 1
+fi
+SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
+STORE_UNIQUE_SCRIPT="$SCRIPT_PATH"/store_unique.sh
+
+ENCODING="utf-8"
+
+# parse command-line options
+DATABASE_FILESPEC="$SCRIPT_PATH"/monkeyplug.db
+while getopts 'd:' OPTION; do
+    case "$OPTION" in
+        d)
+          DATABASE_FILESPEC="$OPTARG"
+          ;;
+
+        ?)
+          echo "script usage: $(basename $0) -d database.db" >&2
+          exit 1
+          ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
 # use monkeyplug, monkeyplug.py or monkeyplug-docker.sh, if available
 MONKEYPLUG_BIN=""
 if command -v monkeyplug >/dev/null; then
@@ -18,23 +45,50 @@ for AUDIOFILE in "$@"; do
   if [[ -f "${AUDIOFILE}" ]] && [[ "${AUDIOFILE}" == *'.'* ]]; then
     pushd "$(dirname "${AUDIOFILE}")" >/dev/null 2>&1
 
-    # monkeyplug will only process audio files that haven't been cleaned before,
-    # based on the contents of embedded metadata tags
-    OUTFILE="${AUDIOFILE%.*}_clean.${AUDIOFILE##*.}"
-    ${MONKEYPLUG_BIN} -i "${AUDIOFILE}" -o "${OUTFILE}" -x MATCH
-
-    # if the cleaned audio file exists and is larger than negligible,
-    # remove the original file and accept the cleaned one. if the output
-    # audio file doesn't exist, something went wrong or it didn't need to
-    # be cleand
-    if [[ -f "${OUTFILE}" ]]; then
-      OUTFILE_SIZE=$(stat -c%s "${OUTFILE}")
-      if (( ${OUTFILE_SIZE} > 16000 )); then
-        rm -f "${AUDIOFILE}"
-        mv -f "${OUTFILE}" "${AUDIOFILE}"
-      fi
+    # we'll use an sqlite database to keep track of scrubbed files, it's faster
+    # than actually checking the file itself
+    if [[ -x "$STORE_UNIQUE_SCRIPT" ]] && [[ -n "$DATABASE_FILESPEC" ]]; then
+      "$STORE_UNIQUE_SCRIPT" \
+        -d "$DATABASE_FILESPEC" \
+        -t "episode" \
+        -f "filename" \
+        -v "$(basename "$AUDIOFILE")" \
+        -o get
+      GET_RESULT=$?
+    else
+      GET_RESULT=1
     fi
 
+    # continue processing as long as we didn't find the filename in the database
+    if [[ "$GET_RESULT" -ne "0" ]]; then
+
+      # monkeyplug will only process audio files that haven't been cleaned before,
+      # based on the contents of embedded metadata tags
+      OUTFILE="${AUDIOFILE%.*}_clean.${AUDIOFILE##*.}"
+      ${MONKEYPLUG_BIN} -i "${AUDIOFILE}" -o "${OUTFILE}" -x MATCH
+
+      # if the cleaned audio file exists and is larger than negligible,
+      # remove the original file and accept the cleaned one. if the output
+      # audio file doesn't exist, something went wrong or it didn't need to be cleand
+      if [[ -f "${OUTFILE}" ]]; then
+        OUTFILE_SIZE=$(stat -c%s "${OUTFILE}")
+        if (( ${OUTFILE_SIZE} > 16000 )); then
+          rm -f "${AUDIOFILE}"
+          mv -f "${OUTFILE}" "${AUDIOFILE}"
+        fi
+
+        # store the resultant file in the database so we don't process it again
+        if [[ -x "$STORE_UNIQUE_SCRIPT" ]] && [[ -n "$DATABASE_FILESPEC" ]]; then
+          "$STORE_UNIQUE_SCRIPT" \
+            -d "$DATABASE_FILESPEC" \
+            -t "episode" \
+            -f "filename" \
+            -v "$(basename "$AUDIOFILE")" \
+            -o set
+        fi
+
+      fi # OUTFILE exists
+    fi # database check
     popd >/dev/null 2>&1
-  fi
+  fi # input audio file exists
 done
---
+++ scripts/store_unique.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# create an SQLite3 database and store a unique value in it
+
+set -e
+set -u
+set -o pipefail
+shopt -s nocasematch
+
+ENCODING="utf-8"
+
+# parse command-line options
+DATABASE_FILESPEC=""
+TABLE_NAME=mesa
+FIELD_NAME=campo
+OPERATION=""
+VALUE=""
+while getopts 'v:d:t:f:o:' OPTION; do
+    case "$OPTION" in
+        v)
+          VALUE="$OPTARG"
+          ;;
+
+        d)
+          DATABASE_FILESPEC="$OPTARG"
+          ;;
+
+        t)
+          TABLE_NAME="$OPTARG"
+          ;;
+
+        f)
+          FIELD_NAME="$OPTARG"
+          ;;
+
+        o)
+          OPERATION="$OPTARG"
+          ;;
+
+        ?)
+          echo "script usage: $(basename $0) -d database.db -t table -f field -v value -o (set|get)" >&2
+          exit 1
+          ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+# cross-platform GNU gnonsense for core utilities
+[[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
+[[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
+if ! (command -v "$REALPATH" && command -v "$DIRNAME" && command -v sqlite3) > /dev/null; then
+    echo "$(basename "${BASH_SOURCE[0]}") requires $REALPATH and $DIRNAME and sqlite3"
+    exit 1
+fi
+SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
+
+if ( [[ "$OPERATION" != "set" ]]  && [[ "$OPERATION" != "get" ]] ) || [[ -z "$VALUE" ]]; then
+    echo "script usage: $(basename $0) -d database.db -t table -f field -v value -o (set|get)" >&2
+    exit 1
+fi
+
+# get database filename and directory to use, and specify a lock directory for a singleton
+[[ -z "$DATABASE_FILESPEC" ]] && DATABASE_FILESPEC="$SCRIPT_PATH"/database.db
+DATABASE_DIR="$($DIRNAME "${DATABASE_FILESPEC}")"
+
+# make sure only one instance of the script
+LOCK_DIR="${DATABASE_DIR}/$(basename "$DATABASE_FILESPEC")_lock"
+function finish {
+    rmdir -- "$LOCK_DIR" || echo "Failed to remove lock directory '$LOCK_DIR'" >&2
+}
+
+if mkdir -- "$LOCK_DIR" 2>/dev/null; then
+    trap finish EXIT
+    ECODE=0
+    pushd "$DATABASE_DIR" >/dev/null 2>&1
+
+    if [[ "$OPERATION" == "set" ]]; then
+        # store an entry in the database
+        sqlite3 "$(basename "$DATABASE_FILESPEC")" <<EOF
+CREATE TABLE IF NOT EXISTS \`$TABLE_NAME\` (id INTEGER PRIMARY KEY, timestamp DATE DEFAULT (datetime('now','localtime')), \`$FIELD_NAME\` text UNIQUE);
+INSERT INTO \`$TABLE_NAME\` (\`$FIELD_NAME\`) VALUES ('$VALUE') ON CONFLICT(\`$FIELD_NAME\`) DO UPDATE SET timestamp=datetime('now','localtime');
+SELECT * FROM \`$TABLE_NAME\` WHERE (\`$FIELD_NAME\` == '$VALUE');
+EOF
+
+    else
+        # retrieve an entry from the database
+        mapfile -t OUTPUT < <(sqlite3 "$(basename "$DATABASE_FILESPEC")" <<EOF
+CREATE TABLE IF NOT EXISTS \`$TABLE_NAME\` (id INTEGER PRIMARY KEY, timestamp DATE DEFAULT (datetime('now','localtime')), \`$FIELD_NAME\` text UNIQUE);
+SELECT * FROM \`$TABLE_NAME\` WHERE (\`$FIELD_NAME\` == '$VALUE');
+EOF
+        )
+        OUTPUT_COUNT=${#OUTPUT[@]}
+        ( [[ -z "$OUTPUT_COUNT" ]] || (( $OUTPUT_COUNT == 0 )) ) && ECODE=1 || printf "%s\n" "${OUTPUT[@]}"
+        (( $ECODE == 0 )) || echo "\"$VALUE\" not found" >&2
+    fi
+
+    popd >/dev/null 2>&1
+    finish
+    trap - EXIT
+    exit $ECODE
+
+else
+  exit 1
+fi # singleton lock check
