850bf2f6218b931814de96a41901e26d6798d06e
Seth Grover <mero.mero.guero@gmail.com>
2022-10-14 13:03:32 -0600

    bump development version

--- src/monkeyplug/__init__.py
+++ src/monkeyplug/__init__.py
@@ -1,6 +1,6 @@
 """monkeyplug is a little script to mute profanity in audio files."""
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Seth Grover <mero.mero.guero@gmail.com>"
 __all__ = []
 
